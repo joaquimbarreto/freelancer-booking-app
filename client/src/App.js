1209b09b68650eecf8e32ef1d395c52fdc8ae8c5
joaquimbarreto <joaquim@barreto.co>
2019-02-19 18:25:20 +0000

    GoogleCalendarApi Component is working

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Calendar from "react-big-calendar";
+import moment from "moment";
+
+import "./App.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import GoogleCalendarApi from "./components/GoogleCalendarApi";
+
+const localizer = Calendar.momentLocalizer(moment);
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		events: [
+			{
+				start: new Date(),
+				end: new Date(moment().add(1, "days")),
+				title: "Freelance Work"
+			}
+		]
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<GoogleCalendarApi events={this.state.events} />
+				<Calendar
+					localizer={localizer}
+					defaultDate={new Date()}
+					defaultView="month"
+					events={this.state.events}
+					style={{ height: "100vh" }}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ client/src/components/GoogleCalendarApi.js
@@ -0,0 +1,159 @@
+import React, { Component } from "react";
+// Client ID and API key from the Developer Console
+const CLIENT_ID =
+	"1005600538488-7g41gt2g6i8uad3ckl397aag0gm67b6v.apps.googleusercontent.com";
+const API_KEY = "AIzaSyADcWwyeoYH7pQruwOQBhkWbuBRuUM3Z_4";
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+const DISCOVERY_DOCS = [
+	"https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"
+];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = "https://www.googleapis.com/auth/calendar";
+
+// const gapi = "https://apis.google.com/js/api.js";
+
+export default class GoogleCalendarApi extends Component {
+	// On load, called to load the auth2 library and API client library.
+	handleClientLoad = () => {
+		window.gapi.load("client:auth2", this.initClient);
+	};
+
+	componentDidMount = () => {
+		this.handleClientLoad();
+	};
+
+	// Initializes the API client library and sets up sign-in state listeners.
+	initClient = () => {
+		window.gapi.client
+			.init({
+				apiKey: API_KEY,
+				clientId: CLIENT_ID,
+				discoveryDocs: DISCOVERY_DOCS,
+				scope: SCOPES
+			})
+			.then(
+				() => {
+					// Listen for sign-in state changes.
+					window.gapi.auth2
+						.getAuthInstance()
+						.isSignedIn.listen(this.updateSigninStatus);
+
+					// Handle the initial sign-in state.
+					this.updateSigninStatus(
+						window.gapi.auth2.getAuthInstance().isSignedIn.get()
+					);
+					// authorizeButton.onclick = this.handleAuthClick;
+					// signoutButton.onclick = this.handleSignoutClick;
+				},
+				error => {
+					this.appendPre(JSON.stringify(error, null, 2));
+				}
+			);
+	};
+
+	// Called when the signed in status changes, to update the UI
+	// appropriately. After a sign-in, the API is called.
+
+	updateSigninStatus = isSignedIn => {
+		var authorizeButton = document.getElementById("authorize_button");
+		var signoutButton = document.getElementById("signout_button");
+
+		if (isSignedIn) {
+			console.log("logged in");
+			authorizeButton.style.display = "none";
+			signoutButton.style.display = "block";
+			this.listUpcomingEvents();
+		} else {
+			console.log("not logged in");
+			authorizeButton.style.display = "block";
+			signoutButton.style.display = "none";
+		}
+	};
+
+	// Sign in the user upon button click.
+
+	handleAuthClick = event => {
+		window.gapi.auth2.getAuthInstance().signIn();
+	};
+
+	// Sign out the user upon button click.
+
+	handleSignoutClick = event => {
+		window.gapi.auth2.getAuthInstance().signOut();
+	};
+
+	/**
+	 * Append a pre element to the body containing the given message
+	 * as its text node. Used to display the results of the API call.
+	 *
+	 * @param {string} message Text to be placed in pre element.
+	 */
+	appendPre = message => {
+		var pre = document.getElementById("content");
+		var textContent = document.createTextNode(message + "\n");
+		pre.appendChild(textContent);
+	};
+
+	/**
+	 * Print the summary and start datetime/date of the next ten events in
+	 * the authorized user's calendar. If no events are found an
+	 * appropriate message is printed.
+	 */
+	listUpcomingEvents = () => {
+		window.gapi.client.calendar.events
+			.list({
+				calendarId: "primary",
+				timeMin: new Date().toISOString(),
+				showDeleted: false,
+				singleEvents: true,
+				maxResults: 10,
+				orderBy: "startTime"
+			})
+			.then(response => {
+				var events = response.result.items;
+				this.appendPre("Upcoming events:");
+
+				if (events.length > 0) {
+					for (var i = 0; i < events.length; i++) {
+						var event = events[i];
+						var when = event.start.dateTime;
+						if (!when) {
+							when = event.start.date;
+						}
+						this.appendPre(event.summary + " (" + when + ")");
+					}
+				} else {
+					this.appendPre("No upcoming events found.");
+				}
+			});
+	};
+
+	render() {
+		return (
+			<div>
+				<p>Google Calendar API Quickstart</p>
+
+				{/* Add buttons to initiate auth sequence and sign out  */}
+				<button
+					id="authorize_button"
+					style={{ display: "none" }}
+					onClick={this.handleAuthClick}
+				>
+					Authorize
+				</button>
+				<button
+					id="signout_button"
+					style={{ display: "none" }}
+					onClick={this.handleSignoutClick}
+				>
+					Sign Out
+				</button>
+
+				<pre id="content" style={{ whiteSpace: "pre-wrap" }} />
+			</div>
+		);
+	}
+}
