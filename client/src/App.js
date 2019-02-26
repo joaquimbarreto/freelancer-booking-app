66aa43b9ce5c8e154b3a8270f524af933ead6b18
joaquimbarreto <joaquim@barreto.co>
2019-02-26 10:54:35 +0000

    material and react-router installed

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,35 @@ import React, { Component } from "react";
 import "./App.css";
 
 import MainCalendar from "./containers/MainCalendar";
+import usersAPI from "./usersAPI";
 
 import { Route, Switch, withRouter } from "react-router-dom";
 
 class App extends Component {
+	state = {
+		username: ""
+	};
+
+	signin = (username, token) => {
+		this.setState({ username });
+		localStorage.setItem("token", token);
+	};
+
+	signout = () => {
+		this.setState({ username: "" });
+		localStorage.removeItem("token");
+	};
+
+	componentDidMount() {
+		usersAPI.validate().then(data => {
+			if (data.error) {
+				this.props.history.push("/signin");
+			} else {
+				this.signin(data.username, data.token);
+				this.props.history.push("/inventory");
+			}
+		});
+	}
 	render() {
 		return (
 			<div className="App">
