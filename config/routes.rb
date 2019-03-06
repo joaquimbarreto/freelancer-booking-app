e3da8d3c237e0bf427ee0c94ace74ae161c6bdff
joaquimbarreto <joaquim@barreto.co>
2019-03-06 13:27:44 +0000

    forgot proxy

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,21 @@ Rails.application.routes.draw do
 
   get '*path', to: "application#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
+  end
+
+
+  scope '/api' do
+    resources :bookings, except: [:destroy, :create]
+    delete 'delete_booking', to: 'bookings#destroy'
+    post 'create_booking', to: 'bookings#create'
+    resources :freelancers
+    resources :users
+    post 'login', to: 'users#login'
+    post 'register', to: 'users#create'
+    get 'validate', to: 'users#validate'
+  
+    get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+      !request.xhr? && request.format.html?
+    end
+  end
 end
